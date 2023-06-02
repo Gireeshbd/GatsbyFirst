1feced953f9f5759281149db6d34c57242632998
Gireeshbd <gireeshreddy1945@gmail.com>
2023-06-02 11:13:25 +0530

    Finished Gatsby Tutoria part 2

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+// Step 1: Import React
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "./layout";
+import { headingAbout } from "../components/layout.module.css";
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <main>
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
+        <Link to="/">Back to home</Link>
+      </main>
+    </Layout>
+  );
+};
+export const Head = () => <title>About</title>;
+// Step 3: Export your component
+export default AboutPage;
